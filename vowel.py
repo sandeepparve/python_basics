01cd6d1c8de04ffd0504a55d8037af116bd5046b
sandeepparve <39026919+sandeepparve@users.noreply.github.com>
2018-06-30 19:41:45 +0530

    Add files via upload

---
+++ vowel.py
@@ -0,0 +1,19 @@
+import os,sys
+def vowel(filename):
+	try:
+		list = []        										#Declaring the list of words.
+		x= open(filename,"rU")
+		y= x.read()     										#reading the text file.
+		lines = y.split()   										#here were splitng the words in paragarphs.
+		for words in lines:
+			if ('a' in words or 'e' in words or 'i' in words or 'o' in words or 'u' in words): 	#compairing words with vowels.
+				list.append(words)								#appending the words with vowels.
+		print list
+		x.close()
+	except:
+		print "file you entered doesn't exists"
+def main():
+	vowel(sys.argv[1])
+if __name__=='__main__':
+	main()
+	 
